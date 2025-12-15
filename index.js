b2fbee7cb6b205068a64109a15b21330a99350b8
Aleksandar Milosheski <73908689+aleks-123@users.noreply.github.com>
2025-12-15 21:51:52 +0100

    FIRST

---
+++ index.js
@@ -0,0 +1,16 @@
+//! NodeJS
+
+//? Што е НоудЏС
+//* НоудЏС е околина во моја можеме да извршуваме јаваскипт код надвор од веб прелистувач
+//* Изработен е врз база на Chrome V8 JavaScript engine
+//? Кои се клучни точки
+//* е single threaded, кој е асихрон код каде што тешките функции се извршуваат во позадина
+
+//!1. - Core moduli
+//*http, assert, fs, path, process, os
+
+//!2 - local moduli
+//* Овие модули ги креираме ние
+
+//!3 - Third-party
+//* mongoose, express, dotenv, ejs, morgan
