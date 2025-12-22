44129ba843dfb7f088d8541e7b0238a82239ed85
Aleksandar Milosheski <73908689+aleks-123@users.noreply.github.com>
2025-12-22 21:43:24 +0100

    c3

--- c3/index.js
+++ c3/index.js
@@ -26,3 +26,8 @@ console.log(rezultat1);
 //todo: Da se kreira fajl so ime vaga.js
 //todo: Da se eskportira funkcija koja kje pretvora gram vo kilogram i obratno
 //todo: Da se esportira funckija koja kje pretvora litar vo mililitar i obratno
+
+//? Za domasna da se dodade uste eden modul so ime domasna.js
+//? koj kje ima funkcija da promeni farenhajt vo celzius i obratno
+//? da ima funkcija koja sto presmetuva plostina i perimetar na pravoagolnik
+//? da ima funkcija sto proveruva dali nekoj broj e paren ili neparen
