8c2696b18cd66a415a7edb741951b0a09af8f27d
Aleksandar Milosheski <73908689+aleks-123@users.noreply.github.com>
2026-02-09 21:56:23 +0100

    123

--- c10/app.js
+++ c10/app.js
@@ -5,40 +5,14 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // app.get("/form");
 // app.get("/form");
 
-const model = require('../models/studenti')
-
-
-
-
-app.get('/brishi/:i. ' (req, res) => {
-  req.params.i
-
-  await model.remove(+req.params.i)
-})
-
-
-
-
-
-
+const model = require("../models/studenti");
 
 app.listen(10000, (err) => {
   if (err) return console.log(error.message);
   console.log("Server successfully started on port 100000");
 });
-
-
-
-ejs.forEach(s, i) => [s.ime, s.prezime, i], [asdgsadg]
-
-a href="/brishi/ <%= i %>"
-a href="/brishi/ <%= i %>"
-a href="/brishi/ <%= i %>"
-a href="/brishi/ <%= i %>"
-a href="/brishi/ <%= i %>"
-a href="/brishi/ <%= i %>"
-
